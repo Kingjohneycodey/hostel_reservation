b54d02fc5246b07f398db761025e816c49b3fed5
kingjohneycodey <kvngjohnny10@gmail.com>
2026-02-20 10:38:31 +0100

    fixed issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hostel_reservation/data_seeder.dart';
+// import 'package:hostel_reservation/data_seeder.dart';
 import 'firebase_options.dart';
 import 'providers/router_provider.dart';
-import 'package:go_router/go_router.dart';
-//import 'package:hostel_reservation/registration_screen.dart';
-import 'package:hostel_reservation/sign_in_screen.dart';
-import 'package:hostel_reservation/splash_screen.dart';
-//import 'data_seeder.dart';
-import 'firebase_options.dart';
-import 'screens/home_screen.dart';
-import 'screens/hostel_list_screen.dart';
-import 'screens/hostel_detail_screen.dart';
-import 'screens/room_selection_screen.dart';
-import 'screens/complaint_page.dart';
-import 'screens/review_page.dart';
-import 'screens/feedback_screen.dart';
-import 'screens/admin/manage_rooms_screen.dart';
-import 'screens/admin/add_edit_room_screen.dart';
-import 'screens/user_profile.dart';
-//import 'sign_in_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
